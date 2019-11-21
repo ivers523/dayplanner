79886665bc645d3aa5ac730db6d5b2ae0ff708be
Michael Iverson <michaeliverson@Michaels-MBP-6.domain>
2019-11-20 23:01:20 -0600

    updated styles, implemented color logic

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,58 @@
-window.onload = function() {
-    
+window.onload = function () {
+
     let now = moment();
     let day = moment().format("MMMM DD YYYY");
-    timeArray = [9,10,11,12,13,14,15,16,17];
+    let hour = moment().format("HH");
+    timeArray = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+    console.log(now);
+    console.log(hour);
 
     // display current date in Jumbotron
-    document.getElementById("currentDay").innerHTML= day;
+    document.getElementById("currentDay").innerHTML = day;
 
-    function colors(){
-        for(i = 0; i < timeArray.length; i++){
-            if (timeArray[i] < now){
-                document.querySelector('#eventbox').setAttributead('class', 'past');
+    // change colors to reflect past, present, and future
+    function colors() {
+        for (i = 0; i < timeArray.length; i++) {
+            if (hour < timeArray[i]) {
+                document.querySelector(".description").setAttribute("class", "past");
+                console.log("past");
 
-            }else if(timeArray[i] > now){
-                document.querySelector('#eventbox').setAttribute('class', 'future');
+            } else if (hour > timeArray[i]) {
+                document.querySelector(".description").setAttribute("class", "future");
+                console.log("future");
 
-            }else if(timeArray[i] === now){
-                document.querySelector('#eventbox').setAttribute('class', 'present');
-            
-            };  
+            } else if (hour === timeArray[i]) {
+                document.querySelector(".description").setAttribute("class", "present");
+                console.log("present");
+
+            };
         };
     };
     colors();
-    moment();
-
-
-
-
-
-
-
-
-
-
-    };
-
-
+    // save textarea events to local storage
 
     
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
