2265a52449b4adf16cc80dc752e2572cb3133643
Michael Iverson <michaeliverson@Michaels-MBP-6.domain>
2019-11-21 18:00:38 -0600

    updated JS to store events in localdata

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 * Displays hour-long blocks of time for a standard work day, from 9:00 AM to 5:00 PM
 * Allows users to save events in local storage for retrieval later
 * Developed 11/2019
+* Live site deployed at  https://ivers523.github.io/dayplanner/
--- assets/script.js
+++ assets/script.js
@@ -6,35 +6,52 @@ window.onload = function () {
     timeArray = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
     console.log(now);
     console.log(hour);
-
     // display current date in Jumbotron
     document.getElementById("currentDay").innerHTML = day;
+    // save textarea events to local storage
+    // Input/set local storage
+    function save() {
+        const saveEl = document.getElementById("save");
+        saveEl.addEventListener("click", function () {
+            const input = document.getElementById("event").value;
+            localStorage.setItem("event", JSON.stringify(input));
+
+        });
+    };
+    save();
+
+    function retrieve() {
+
+    }
 
     // change colors to reflect past, present, and future
     function colors() {
         for (i = 0; i < timeArray.length; i++) {
             if (hour < timeArray[i]) {
                 document.querySelector(".description").setAttribute("class", "future");
-                console.log("past");
+
 
             } else if (hour > timeArray[i]) {
                 document.querySelector(".description").setAttribute("class", "past");
-                console.log("future");
+
 
             } else if (hour === timeArray[i]) {
                 document.querySelector(".description").setAttribute("class", "present");
-                console.log("present");
+
 
             };
         };
     };
     colors();
-    // save textarea events to local storage
-
-    
 
 
-    }
+
+
+    //
+
+
+
+}
 
 
 
